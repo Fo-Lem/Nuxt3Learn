597d86d03e4c2f663824f7324977c7843ab03906
Fo-lem <darksunset2001@gmail.com.com>
2023-11-10 19:29:00 +0300

    feat: update TheHeader component

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,66 +1,173 @@
 <script setup>
+import { ref } from 'vue'
+import Burger from './icons/burger.vue'
 
+const showMobileMenu = ref(false)
 </script>
 
 <template>
   <header class="header">
-    <nav class="menu">
-      <NuxtLink class="menu__logo" to="/">
+    <h1 class="header__logo">
+      <NuxtLink class="header__logo-link" to="/">
         CarPrice
       </NuxtLink>
+    </h1>
+    <button class="burger">
+      <Burger class="burger-icon" :height="30" :width="30" @click="showMobileMenu = !showMobileMenu" />
+    </button>
+    <nav class="menu">
       <ul class="menu__list">
-        <li>
-          <NuxtLink class="menu__list-link" to="/about">
-            About
+        <li class="menu__item">
+          <NuxtLink class="menu__item-link" to="/products">
+            Товары
           </NuxtLink>
         </li>
-        <li>
-          <NuxtLink class="menu__list-link" to="/products">
-            Products
+        <li class="menu__item">
+          <NuxtLink class="menu__item-link" to="/about">
+            О нас
+          </NuxtLink>
+        </li>
+        <li class="menu__item">
+          <NuxtLink class="menu__item-link" to="/profile">
+            Личный кабинет
           </NuxtLink>
         </li>
       </ul>
     </nav>
+    <dialog class="show-mobile" :open="showMobileMenu">
+      <nav class="mobile-menu">
+        <ul class="mobile-menu__list">
+          <li class="mobile-menu__item">
+            <NuxtLink class="mobile-menu__item-link" to="/products">
+              Товары
+            </NuxtLink>
+          </li>
+          <li class="mobile-menu__item">
+            <NuxtLink class="mobile-menu__item-link" to="/about">
+              О нас
+            </NuxtLink>
+          </li>
+          <li class="mobile-menu__item">
+            <NuxtLink class="mobile-menu__item-link" to="/profile">
+              Личный кабинет
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </dialog>
   </header>
 </template>
 
 <style lang="scss" scoped>
 .header {
-  width: 100%;
-  box-shadow: 0 0 5px rgb(0 0 0 / 20%);
-
-}
-
-.menu {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: $padding-default $padding-default;
+  width: 100%;
+  height: 80px;
+  padding: $padding-default;
+  box-shadow: 0 0 5px rgb(0 0 0 / 20%);
 
-  &__logo {
-    padding: $padding-default * 0.5  $padding-default * 2;
-    color: $btn-text-default;
-    font-size: 28px;
-    border-radius: $border-radius-default;
+  &__logo{
+    font-weight: 700;
+    font-size: 24px;
+
+    &-link{
+      padding: $padding-default * 0.5 $padding-default * 2;
+      border-radius: $border-radius-default;
+    }
   }
 
-  &__list {
-    display: flex;
-    gap: $gap-default;
-    margin: 0;
+  .menu {
+    &__list {
+      display: flex;
+      gap: $gap-default * 2;
+      align-items: center;
+    }
 
-    &-link {
+    &__item {
       display: block;
-      padding: $padding-default $padding-default * 2;
-      color: $btn-text-default;
-      border-radius: $border-radius-default;
+
+      &-link {
+        display: inline-block;
+        padding: $padding-default $padding-default * 2;
+        border-radius: $border-radius-default;
+      }
+    }
+
+    @media screen and (width <= 768px) {
+      display: none;
+
+    }
+  }
+
+  .show-mobile{
+    top: 80px - $padding-default * 2;
+    left: 0;
+    width: 100%;
+    box-shadow: 0 3px 5px rgb(0 0 0 / 10%);
+
+  }
+
+  .mobile-menu {
+    &__list {
+      display: flex;
+      flex-direction: column;
+      gap: $gap-default;
+      align-items: center;
+      padding: $padding-default;
+    }
+
+    &__item {
+      display: block;
+      width: 100%;
+
+      &-link {
+        display: inline-block;
+        width: 100%;
+        padding: $padding-default $padding-default * 2;
+        border-radius: $border-radius-default;
+      }
+    }
+
+    @media screen and (width >= 768px) {
+      display: none;
+
     }
 
   }
 
+  .burger {
+    display: none;
+    justify-content: center;
+    padding: $padding-default * 0.5;
+    background-color: #fff;
+    border: none;
+    border-radius: $border-radius-default;
+    cursor: pointer;
+
+    &-icon{
+      stroke: black;
+    }
+
+    &:hover {
+      background-color: $btn-bg-hover;
+
+      .burger-icon{
+      stroke: white;
+    }
+    }
+
+    @media screen and (width <= 768px) {
+      display: flex;
+
+    }
+  }
+
+}
+
   .router-link-exact-active {
     color:$btn-text-active;
     background-color: $btn-bg-active;
-}
 }
 </style>
